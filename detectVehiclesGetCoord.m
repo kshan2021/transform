883cd0b40e8121f048aeb55ed59cf68714ef6c76
kshan2021 <94374050+kshan2021@users.noreply.github.com>
2022-02-15 03:56:12 +0000

    Create detectVehiclesGetCoord.m
    [transform] MATLAB call YOLO, detect vehicles from tmp.jpg and return results

---
+++ detectVehiclesGetCoord.m
@@ -0,0 +1,20 @@
+function vehicles = detectVehiclesGetCoord
+oldDir = pwd;
+cd('~/GitHub/darknet');
+
+system("./darknet detector test ./cfg/coco.data ./training/t0907s-c80-b10000_2021-12-03/yolov4-t0907s-c80-b10000.cfg ./training/t0907s-c80-b10000_2021-12-03/yolov4-t0907s-c80-b10000_last.weights -dont_show -ext_output tmp.jpg -out tmp.json ");
+
+inImage = imread('tmp.jpg');
+[h, w, c] = size(inImage);
+
+inFile = 'tmp.json';
+fid = fopen(inFile);
+raw = fread(fid, inf);
+str = char(raw');
+fclose (fid);
+jsonStr = jsondecode(str);
+vehicles = jsonStr.objects;
+
+cd(oldDir);
+end
+
