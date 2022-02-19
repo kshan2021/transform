e33e5afabb3c13d3c31ca43ffac4ca84165c7c0d
kshan2021 <94374050+kshan2021@users.noreply.github.com>
2022-02-19 18:02:51 +0000

    Update Transform.m
    [transform] update source image path Ch10

--- Transform.m
+++ Transform.m
@@ -31,7 +31,7 @@ imgFilename{6} = rootDir + "Data Annotation/Datasets/e0804-dataset/Annotated-Man
 imgFilename{7} = rootDir + "Data Annotation/Datasets/e0804-dataset/Annotated-Manual-Ground-Truth-e0804/2021-06-05-1100/NVR_ch7_main_20210605110003_20210605120003-vlcsnap-2021-07-21-11h05m40s555.jpg";
 imgFilename{8} = rootDir + "Data Annotation/Datasets/e0804-dataset/Annotated-Manual-Ground-Truth-e0804/2021-06-05-1100/NVR_ch8_main_20210605110003_20210605120003-vlcsnap-2021-07-21-11h04m34s856.jpg";
 imgFilename{9} = rootDir + "Data Annotation/Video Annotation/ch9_vlcsnap-2022-02-17-11h33m39s755.jpg";
-imgFilename{10}= rootDir + "Data Annotation/Datasets/t0824-dataset/Annotated-Manual-Ground-Truth-t0824/2021-07-15-1000/NVR_ch10_main_20210715100004_20210715110004.mp4-vlcsnap-2021-08-22-22h02m57s188.jpg";
+imgFilename{10}= rootDir + "Data Annotation/Datasets/t0824-dataset/Annotated-Manual-Ground-Truth-t0824s/2021-07-15-1000/NVR_ch10_main_20210715100004_20210715110004.mp4-vlcsnap-2021-08-22-22h02m57s188.jpg";
 % ch9 ch10 images size changed after e0804 collected.
 
 [~, total] = size(imgFilename);
